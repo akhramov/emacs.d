49a1f602c8c244a6753102c128333781f0df00cf
Artem Khramov <futu.fata@gmail.com>
2017-07-11 21:12:35 +0600

    Config updates
    Add :ensure keyword for external packages, remove helm-projectile-ag
    declaration, since it's a part of helm-projectile

--- init.el
+++ init.el
@@ -38,47 +38,53 @@ Path is built by appending SEGMENTS to 'default-directory'"
   ("C-x b" . akhramov-projectile-switch-to-buffer))
 
 ;; Helm
-(use-package helm)
+(use-package helm
+  :ensure t)
 
 (use-package helm-projectile
+  :ensure t
   :bind
-  ("C-x C-p" . helm-projectile))
+  ("C-x C-p" . helm-projectile)
+  ("C-x C-o" . helm-projectile-ag))
 
 (use-package helm-mt
+  :ensure t
   :bind
   ("C-x p" . helm-mt))
 
-(use-package helm-projectile-ag
-  :bind
-  ("C-x C-o" . helm-projectile-ag))
-
 ;; Comment util
 (use-package comment-dwim-2
+  :ensure t
   :bind
   ("M-;" . comment-dwim-2))
 
 ;; Multiple cursors
 (use-package multiple-cursors
+  :ensure t
   :bind
   ("M-]" . mc/mark-next-like-this))
 
 (use-package multi-term
+  :ensure t
   :init
   (setf multi-term-program (executable-find "fish"))
   (setf system-uses-terminfo nil))
 
 ;; Flycheck
 (use-package flycheck
+  :ensure t
   :init
   (add-hook 'after-init-hook #'global-flycheck-mode))
 
 ;; Ruby mode
 (use-package enh-ruby-mode
+  :ensure t
   :init
   (add-hook 'ruby-mode-hook 'enh-ruby-mode))
 
 ;; JS2 mode
 (use-package js2-mode
+  :ensure t
   :init
   (add-hook 'js-mode-hook 'js2-minor-mode))
 
