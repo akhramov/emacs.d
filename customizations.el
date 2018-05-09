91fff70b26ea26653409623d9c829c40038d5ada
Artyom Khramov <futu.fata@gmail.com>
2018-05-09 20:29:16 +0600

    Config updates
    * Add `exec-path-from-shell` package
    * Add projectile-swith-project-actions which simply opens the last
      openned buffer or project root in lack thereof.
    * Unstable melpa is the best melpa
    * Various customizations.el updates (should get rid of this file
      eventually).

--- customizations.el
+++ customizations.el
@@ -13,6 +13,7 @@
    '("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "9541f1dc11258239ef02aa1a5e9db3e1e46bc8fb1d7dbe83946c1541ae6dbdf9" default))
  '(desktop-restore-frames t)
  '(enh-ruby-deep-indent-paren nil)
+ '(epa-pinentry-mode 'loopback)
  '(eyebrowse-keymap-prefix "")
  '(flycheck-disabled-checkers '(c/c++-clang ruby-rubylint))
  '(flycheck-gcc-include-path '("/Users/artem/.rbenv/versions/2.4.2/include/ruby-2.4.0"))
@@ -25,7 +26,7 @@
  '(org-todo-keywords '((sequence "TODO" "IN PROGRESS" "DONE")))
  '(org-wild-notifier-keyword-blacklist nil)
  '(package-selected-packages
-   '(eclim cider ob-restclient docker-tramp org-wild-notifier dash-functional dash org-alert htmlize desktop+ haskell-mode vue-mode window-jump reverse-im suscolors-theme ample-theme helm-mt yaml-mode wanderlust w3m use-package tern spacemacs-theme spaceline scss-mode sass-mode ruby-additional racket-mode php-mode persp-projectile org neotree mustache-mode multiple-cursors multi-term mu4e-maildirs-extension mu4e-alert minitest magit lua-mode leuven-theme js2-mode ivy indent-guide helm-projectile helm-ag ghc geiser flymd flycheck exec-path-from-shell erlang enh-ruby-mode elm-mode dtrace-script-mode comment-dwim-2 column-marker color-theme-sanityinc-solarized color-theme coffee-mode ascii-art-to-unicode alchemist ag ack-menu ack))
+   '(cider pinentry system-packages eclim ob-restclient docker-tramp org-wild-notifier dash-functional dash org-alert htmlize desktop+ haskell-mode vue-mode window-jump reverse-im suscolors-theme ample-theme helm-mt yaml-mode wanderlust w3m use-package tern spacemacs-theme spaceline scss-mode sass-mode ruby-additional racket-mode php-mode persp-projectile org neotree mustache-mode multiple-cursors multi-term mu4e-maildirs-extension mu4e-alert minitest magit lua-mode leuven-theme js2-mode ivy indent-guide helm-projectile helm-ag ghc geiser flymd flycheck exec-path-from-shell erlang enh-ruby-mode elm-mode dtrace-script-mode comment-dwim-2 column-marker color-theme-sanityinc-solarized color-theme coffee-mode ascii-art-to-unicode alchemist ag ack-menu ack))
  '(safe-local-variable-values
    '((eval when
            (and
@@ -81,6 +82,7 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "#000000" :foreground "#d3d3d3" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "nil" :family "DejaVu Sans Mono for Powerline"))))
  '(magit-section-highlight ((t nil)))
+ '(mmm-default-submode-face ((t nil)))
  '(org-block ((t (:background "black" :foreground "#dcdccc"))))
  '(org-block-begin-line ((t (:background "black" :foreground "#008ED1"))))
  '(org-block-end-line ((t (:background "black" :foreground "#008ED1")))))
